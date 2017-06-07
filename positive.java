87a29efffa7ad54ffa1e9cc78753d5fd0d366d7e
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-07 10:04:25 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+class positive
+{
+ public static void main(String arg[])
+ {
+  int i=Integer.parseInt(arg[0]);
+  if(i!=0)
+  {
+    if(i>0)
+    System.out.println("positive");
+    else
+     System.out.println("negative");
+   }
+   else
+    System.out.println("zero");
+  }
+}
