1b2c1e69c13b7b093083c885e824d3be58479a3f
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-08 10:12:13 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+class oddeven
+{
+ public static void main(String ar[])
+ {
+  int i=Integer.parseInt(ar[0]);
+  if(i%2==0)
+  System.out.println("even");
+  else
+  System.out.println("odd");
+ }
+}
