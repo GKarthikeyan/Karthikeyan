d8272f01749eb7df0bc88200fc47999346e4b451
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-08 21:44:28 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,11 @@
+class leapyear
+{
+    public static void main(String ar[])
+    {
+        int i=Integer.parseInt(ar[0]);
+        if(i%4==0)
+        System.out.println("leap year");
+        else
+        System.out.println("not a leap year");
+    }
+}
