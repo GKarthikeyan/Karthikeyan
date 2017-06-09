bd6107f59e6a3f3597079ba1b3a49ea6149767cd
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-09 10:44:40 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,12 @@
+class natural
+{
+ public static void main(String ar[])
+ {
+  int sum=0;
+  int in=Integer.parseInt(ar[0]);
+  for(int i=1;i<=in;i++)
+  sum=sum+i;
+  
+  System.out.println(sum);
+ }
+}
