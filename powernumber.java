26bd2a4b5b8ec3a923b2595d54735740b335bec6
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-13 12:01:01 +0530

    Create powernumber.java

---
+++ powernumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class powernumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int inp_no=sc.nextInt();
+		sc.nextLine();
+		int power=sc.nextInt();
+		double sum=0;
+		System.out.print(sum=Math.pow(inp_no, power));
+
+	}
+
+}
