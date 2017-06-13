088199690c94663e4a300d010ba226a62e933c60
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-13 09:26:35 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test 
+{	                   
+  public static void main(String[] args)
+  {
+		Scanner sc=new Scanner(System.in);
+		String  n=sc.next();
+    int no=n.length();
+	  System.out.print(no);
+
+	}
+
+}
