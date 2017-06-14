3c54c89bf7e28c27352e2055572cb61e5d9804e4
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-14 12:14:02 +0530

    Create str_to_int.java

---
+++ str_to_int.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class str_to_int {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String no=sc.nextLine();
+		int i=Integer.parseInt(no);
+		System.out.print(i);
+sc.close();
+	}
+
+}
