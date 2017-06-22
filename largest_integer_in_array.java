f6198ebfa77174f19353ab6129b8ec71012c2d88
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-06-22 12:15:35 +0530

    Create largest_integer_in_array.java

---
+++ largest_integer_in_array.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Logic1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int input[]=new int [n];
+		for(int i=0;i<n;i++)
+		{
+			input[i]=sc.nextInt();
+		}
+		Arrays.sort(input);
+		
+		System.out.println(input[n-1]);
+	}
+
+}
