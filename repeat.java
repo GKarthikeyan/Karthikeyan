65e22e0a07273e0e5bc9e7b9c161a9c30c275747
GKarthikeyan <karthikeyanvis1996@gmail.com>
2017-07-19 21:37:38 +0530

    Update repeat.java

--- repeat.java
+++ repeat.java
@@ -7,12 +7,18 @@ public static void main(String ar[])
 	int a[]=new int[n+1];
 	for(int i=0;i<n;i++)
 	a[i]=sc.nextInt();
-	
+	int f=0;
 	Arrays.sort(a);
 	for(int i=0;i<n;i++)
 	{
 		if(a[i]==a[i+1])
+		{
+			f=1;
 			System.out.println(a[i]);
+		}
+		
 	}
+		if(f!=1)
+		System.out.println("No repeat numbers");
 }
 }
